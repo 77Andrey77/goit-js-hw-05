2da4d356e3e7cdbacd6dd6795b99bf2cd854de5e
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2022-07-14 15:48:12 +0300

    task3

--- js/task3.js
+++ js/task3.js
@@ -5,17 +5,24 @@ class Storage  {
     
   }
 
-  // getItems  () {
-  //   items;
-  // };
+  getItems  (items) {
+    return this.items;
+  };
 
-  // addItem (item) {
-    
-  // };
+  addItem (item) {
+    return this.items.push(item);
+  };
 
-  // removeItem (item) {
-    
-  // }
+  removeItem (item) {
+    // for (let i = 0; i < this.items.length; i += 1){
+    //   if (this.items[i] === item) {
+    //     this.items.splice(i, 1);
+    //   }
+    // }
+    if (this.items.indexOf(item) > 0) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
 }
 
 
